8ed339667625cc9a930f03c6842d22bb3d8f379b
DeV <57655776+pateldev2511@users.noreply.github.com>
2023-01-28 11:46:51 -0500

    Update smartlearn.sql

--- smartlearn.sql
+++ smartlearn.sql
@@ -194,10 +194,10 @@ CREATE TABLE IF NOT EXISTS `staff` (
 --
 
 INSERT INTO `staff` (`st_id`, `st_name`, `st_email`, `st_password`, `st_mobile`, `st_address`, `st_pincode`, `st_document`, `st_create`, `st_update`, `st_token`, `st_active`) VALUES
-(1, 'Max Willam', 'max.willam@gmail.com', 'db3b852a890b67b15bf98dfde832d2b5', '8320142418', 'Block-18, Bldg 03, Kapadia Indl Est, 123-124 Andheri Kurla K Rd, Andheri (e)', 384004, 'images.png', '2021-02-16 08:41:14', '2021-03-24 12:53:42', NULL, 'yes'),
-(2, 'Mikel Jorden', 'mikeljorden123@gmail.com', '25f9e794323b453885f5181f1b624d0b', '9409244271', 'Block-18, Bldg 03, Kapadia Indl Est, 123-124 Andheri Kurla K Rd, Andheri (e)', 384009, '6a2523959209d902adc540345120f22d.jpg', '2021-02-16 08:49:48', '2021-03-24 12:53:39', NULL, 'yes'),
-(4, 'Luis Miller', 'luis.miller@gmail.com', '25f9e794323b453885f5181f1b624d0b', '9409244271', 'Akshya Nagar 1st Block 1st Cross, Rammurthy nagar, Bangalore', 560016, 'simple_luis.jpg', '2021-02-17 09:45:12', '2021-02-17 15:15:12', NULL, 'yes'),
-(6, 'Dev Patel', 'pateldev0306@gmail.com', 'f2f0c254b80c4eeab291b95e2ae3c37a', '8320142418', '55, Opp Toyota Showroom, Vinayak Apt, Link Road, Malad (west)', 384001, 'simple_luis.jpg', '2021-03-26 16:58:21', '2021-03-27 15:54:28', '', 'yes');
+(1, 'Max Willam', 'max.willam@gmail.com', 'db3b852a890b67b15bf98dfde832d2b5', '1234567890', 'Block-18, Bldg 03, Kapadia Indl Est, 123-124 Andheri Kurla K Rd, Andheri (e)', 384004, 'images.png', '2021-02-16 08:41:14', '2021-03-24 12:53:42', NULL, 'yes'),
+(2, 'Mikel Jorden', 'mikeljorden123@gmail.com', '25f9e794323b453885f5181f1b624d0b', '1234567890', 'Block-18, Bldg 03, Kapadia Indl Est, 123-124 Andheri Kurla K Rd, Andheri (e)', 384009, '6a2523959209d902adc540345120f22d.jpg', '2021-02-16 08:49:48', '2021-03-24 12:53:39', NULL, 'yes'),
+(4, 'Luis Miller', 'luis.miller@gmail.com', '25f9e794323b453885f5181f1b624d0b', '1234567890', 'Akshya Nagar 1st Block 1st Cross, Rammurthy nagar, Bangalore', 560016, 'simple_luis.jpg', '2021-02-17 09:45:12', '2021-02-17 15:15:12', NULL, 'yes'),
+(6, 'Dev Patel', 'pateldev0306@gmail.com', 'f2f0c254b80c4eeab291b95e2ae3c37a', '1234567890', '55, Opp Toyota Showroom, Vinayak Apt, Link Road, Malad (west)', 384001, 'simple_luis.jpg', '2021-03-26 16:58:21', '2021-03-27 15:54:28', '', 'yes');
 
 -- --------------------------------------------------------
 
@@ -226,18 +226,18 @@ CREATE TABLE IF NOT EXISTS `student` (
 --
 
 INSERT INTO `student` (`s_id`, `s_name`, `s_address`, `s_pincode`, `s_email`, `s_password`, `s_document`, `s_active`, `s_token`, `s_create`, `s_update`) VALUES
-(4, 'Dev Patel', 'Block-18, Bldg 03, Kapadia Indl Est, 123-124 Andheri Kurla K Rd, Andheri (e)', 384002, 'pateldev081999@gmail.com', 'ae46b75e99acd5e581308416e05293fe', 'images.png', 'yes', '', '2021-02-15 08:04:20', NULL),
-(5, 'Darsh Patel', 'Block-18, Bldg 03, Kapadia Indl Est, 123-124 Andheri Kurla K Rd, Andheri (e)', 384001, 'pateldarsh422@gmail.com', 'a7bca7506cdd0eae3a7d2764f1ed9085', 'wallhaven-w8xwpx.jpg', 'yes', '', '2021-02-15 08:08:32', NULL),
-(7, 'Poonam Prajapati', 'Block-18, Bldg 03, Kapadia Indl Est, 123-124 Andheri Kurla K Rd, Andheri (e)', 384001, 'poonamoza10299@gmail.com', '25f9e794323b453885f5181f1b624d0b', 'steam_profile_design___hinata_and_naruto_by_robertorevolution_ddn9nuf.png', 'yes', '', '2021-02-15 08:11:34', NULL),
+(4, 'Dev Patel', 'Block-18, Bldg 03, Kapadia Indl Est, 123-124 Andheri Kurla K Rd, Andheri (e)', 384002, 'abcd123@gmail.com', 'ae46b75e99acd5e581308416e05293fe', 'images.png', 'yes', '', '2021-02-15 08:04:20', NULL),
+(5, 'Darsh Patel', 'Block-18, Bldg 03, Kapadia Indl Est, 123-124 Andheri Kurla K Rd, Andheri (e)', 384001, 'abcd123@gmail.com', 'a7bca7506cdd0eae3a7d2764f1ed9085', 'wallhaven-w8xwpx.jpg', 'yes', '', '2021-02-15 08:08:32', NULL),
+(7, 'Poonam Prajapati', 'Block-18, Bldg 03, Kapadia Indl Est, 123-124 Andheri Kurla K Rd, Andheri (e)', 384001, 'abcd123@gmail.com', '25f9e794323b453885f5181f1b624d0b', 'steam_profile_design___hinata_and_naruto_by_robertorevolution_ddn9nuf.png', 'yes', '', '2021-02-15 08:11:34', NULL),
 (8, 'Shivam', 'Block-18, Bldg 03, Kapadia Indl Est, 123-124 Andheri Kurla K Rd, Andheri (e)', 384002, 'abc@gmail.com', '25f9e794323b453885f5181f1b624d0b', 'wallhaven-ym1wp7.jpg', 'yes', '', '2021-02-16 08:08:07', NULL),
-(10, 'Vishal Modi', 'Block-18, Bldg 03, Kapadia Indl Est, 123-124 Andheri Kurla K Rd, Andheri (e)', 384002, 'modivishal123@gmail.com', '25f9e794323b453885f5181f1b624d0b', '2794b585f834468939ce843cdde3cb55.jpg', 'yes', '', '2021-02-16 10:12:59', NULL),
+(10, 'Vishal Modi', 'Block-18, Bldg 03, Kapadia Indl Est, 123-124 Andheri Kurla K Rd, Andheri (e)', 384002, 'abcd123@gmail.com', '25f9e794323b453885f5181f1b624d0b', '2794b585f834468939ce843cdde3cb55.jpg', 'yes', '', '2021-02-16 10:12:59', NULL),
 (11, 'Dax Shah', 'Akshya Nagar 1st Block 1st Cross, Rammurthy nagar, Bangalore.', 560016, 'daxshah123@gmail.com', '25f9e794323b453885f5181f1b624d0b', 'images.png', 'no', '', '2021-02-18 07:25:07', NULL),
-(12, 'Max Willam', 'Block-18, Bldg 03, Kapadia Indl Est, 123-124 Andheri Kurla K Rd, Andheri (e)', 382001, 'pateldarsh422@gmailc.om', '25f9e794323b453885f5181f1b624d0b', 'images.png', 'no', '502754-602f3a0db40c6', '2021-02-19 04:09:49', NULL),
-(18, 'Recon', 'awgdyauiwvgdbaujhwvdyuawv', 384002, 'darshpatel820@gmail.com', '25f9e794323b453885f5181f1b624d0b', '6a2523959209d902adc540345120f22d.jpg', 'yes', '', '2021-02-19 04:59:51', NULL),
-(19, 'Recon Blaze', 'liawgduiwagdawgdiug', 384001, 'kitel58402@seacob.com', '25f9e794323b453885f5181f1b624d0b', '2794b585f834468939ce843cdde3cb55.jpg', 'no', '910949-602f4a80bac5e', '2021-02-19 05:20:00', NULL),
-(20, 'Recon temp', 'kjbefiusbefuiseuifb', 384001, 'jwngxyembxnnti@maxresistance.com', 'e807f1fcf82d132f9bb018ca6738a19f', '2794b585f834468939ce843cdde3cb55.jpg', 'yes', '', '2021-02-19 05:21:18', NULL),
-(36, 'Dev Patel', '55, Opp Toyota Showroom, Vinayak Apt, Link Road, Malad (west)', 384001, 'pateldev0306@gmail.com', '25f9e794323b453885f5181f1b624d0b', 'images.png', 'yes', '', '2021-03-27 06:50:05', NULL),
-(37, 'Hiral Patel', 'ejhdbuyavdyuvduvauev', 384002, 'patelhinalj@gmail.com', '25f9e794323b453885f5181f1b624d0b', 'images.png', 'no', '984900-605ee0f623703', '2021-03-27 07:38:30', NULL);
+(12, 'Max Willam', 'Block-18, Bldg 03, Kapadia Indl Est, 123-124 Andheri Kurla K Rd, Andheri (e)', 382001, 'abcd123@gmail.com', '25f9e794323b453885f5181f1b624d0b', 'images.png', 'no', '502754-602f3a0db40c6', '2021-02-19 04:09:49', NULL),
+(18, 'Recon', 'awgdyauiwvgdbaujhwvdyuawv', 384002, 'abcd123@gmail.com', '25f9e794323b453885f5181f1b624d0b', '6a2523959209d902adc540345120f22d.jpg', 'yes', '', '2021-02-19 04:59:51', NULL),
+(19, 'Recon Blaze', 'liawgduiwagdawgdiug', 384001, 'abcd123@gmail.com', '25f9e794323b453885f5181f1b624d0b', '2794b585f834468939ce843cdde3cb55.jpg', 'no', '910949-602f4a80bac5e', '2021-02-19 05:20:00', NULL),
+(20, 'Recon temp', 'kjbefiusbefuiseuifb', 384001, 'abcd123@gmail.com', 'e807f1fcf82d132f9bb018ca6738a19f', '2794b585f834468939ce843cdde3cb55.jpg', 'yes', '', '2021-02-19 05:21:18', NULL),
+(36, 'Dev Patel', '55, Opp Toyota Showroom, Vinayak Apt, Link Road, Malad (west)', 384001, 'abcd123@gmail.com', '25f9e794323b453885f5181f1b624d0b', 'images.png', 'yes', '', '2021-03-27 06:50:05', NULL),
+(37, 'Hiral Patel', 'ejhdbuyavdyuvduvauev', 384002, 'abcd123@gmail.com', '25f9e794323b453885f5181f1b624d0b', 'images.png', 'no', '984900-605ee0f623703', '2021-03-27 07:38:30', NULL);
 
 -- --------------------------------------------------------
 
